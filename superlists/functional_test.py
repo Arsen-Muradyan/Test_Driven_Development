562fe39b5935d49b134323ea176970581bf05db7
Arsen Muradyan <arsenmuradyan604@gmail.com>
2021-07-29 15:25:36 +0400

    Extend functional test (test for more items), add some unit test, and changes in view

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 PATH="C:\Program Files (x86)\chromedriver.exe"
 
@@ -13,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     self.browser = webdriver.Chrome(executable_path=PATH, options=options)
   def tearDown(self) -> None:
     self.browser.quit()
+  def check_for_row_in_list_table(self, row_text):
+    table = self.browser.find_element_by_id("id_list_table")
+    rows = table.find_elements_by_tag_name("tr")
+    self.assertIn(row_text, [row.text for row in rows])  
+
   def test_can_start_a_list_and_retrieve_it_later(self):
     # John listen about new cool to-do app. He goes 
     # to check page
@@ -32,12 +38,21 @@ class NewVisitorTest(unittest.TestCase):
     # He press enter and page lists updated
     # "1: Buy peacock feathers" as an item in a to-do list table
     inputbox.send_keys(Keys.ENTER)
+    time.sleep(2)
+    
+    self.check_for_row_in_list_table("1: Buy peacock feathers")
 
-    table = self.browser.find_element_by_id("id_list_table")
-    rows = table.find_elements_by_tag_name("tr")
-    self.assertTrue(
-      any(row.text == "1: Buy peacock feathers" for row in rows)
-    )    
+    # There is still text box inviting her to another item
+    # He enters Use peacock feathers to make a fly" (Edith is very
+    # methodical)
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Use peacock feathers to make a fly')
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(2)
+
+    self.check_for_row_in_list_table('1: Buy peacock feathers')
+    self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
     self.fail("Finish the test")
   
 if __name__ == "__main__":
