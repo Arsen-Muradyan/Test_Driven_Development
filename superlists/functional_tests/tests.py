b305029acb9d31b454adb6f924617890afe97272
Arsen Muradyan <arsenmuradyan604@gmail.com>
2021-07-30 19:57:28 +0400

    make functional_tests an app, use LiveServerTestCase

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,3 +1,4 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -5,8 +6,7 @@ import time
 
 PATH="C:\Program Files (x86)\chromedriver.exe"
 
-
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
   def setUp(self) -> None:
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -22,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
   def test_can_start_a_list_and_retrieve_it_later(self):
     # John listen about new cool to-do app. He goes 
     # to check page
-    self.browser.get('http://localhost:8000')
+    self.browser.get(self.live_server_url)
 
     # She notices the page title and header mention to-do lists
     self.assertIn("To-Do", self.browser.title)
